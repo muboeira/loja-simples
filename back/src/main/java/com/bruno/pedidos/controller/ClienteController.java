f6be4748669e7811ed5b00a6ba0d8af64a6ae9e3
Bruno Alves <souzalves50@gmail.com>
2021-11-22 20:53:02 -0300

    Ajuste rota de clientes

--- back/src/main/java/com/bruno/pedidos/controller/ClienteController.java
+++ back/src/main/java/com/bruno/pedidos/controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.bruno.pedidos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.bruno.pedidos.model.Cliente;
+import com.bruno.pedidos.model.HttpResponse;
 import com.bruno.pedidos.repository.ClienteRepository;
 
 @RestController
@@ -25,32 +27,88 @@ public class ClienteController {
     private ClienteRepository clienteRepository;
     
     @GetMapping("/")
-    public List<Cliente> listar() {
-        return clienteRepository.findAll();
+    public ResponseEntity<?> listar() {
+        HttpResponse response = new HttpResponse();
+        response.setStatus(HttpStatus.OK);
+
+        return new ResponseEntity<>(clienteRepository.findAll(), response.getStatus());
     }
 
     @GetMapping("/{id}")
-    public Optional<Cliente> listarPorId(@PathVariable("id") Long id) {
-        Optional<Cliente> cliente = clienteRepository.findById(id);
-        return cliente;
+    public ResponseEntity<?> listarPorId(@PathVariable("id") Long id) {
+        HttpResponse response = new HttpResponse();
+
+        try {
+            Optional<Cliente> cliente = clienteRepository.findById(id);
+            response.setStatus(HttpStatus.OK);
+            
+            return new ResponseEntity<>(cliente, response.getStatus());
+        }catch(Exception e) {
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+    
+            return new ResponseEntity<>(e.getMessage(), response.getStatus());
+        }
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente cadastrar(@RequestBody Cliente cliente) {
-        return clienteRepository.save(cliente); 
+    public ResponseEntity<?> cadastrar(@RequestBody Cliente cliente) {
+        HttpResponse response = new HttpResponse();
+
+        try {
+            clienteRepository.save(cliente);
+            response.setStatus(HttpStatus.CREATED);
+            response.setMessage("Cliente cadastrado com sucesso.");
+
+            return new ResponseEntity<>(response, response.getStatus());
+        }catch(Exception e) {
+            //talvez tratar erros com funções customizadas nos arquivos repository, ex: nome duplicado
+            //response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            //response.setMessage("Erro ao cadastrar o produto " + produto.getDescricao() + ". Motivo: Produto com esse nome já existe!");
+
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+
+            return new ResponseEntity<>(e.getMessage(), response.getStatus());
+        }
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente atualizar(@RequestBody Cliente cliente) {
-        return clienteRepository.save(cliente); 
+    public ResponseEntity<?> atualizar(@RequestBody Cliente cliente) {
+        HttpResponse response = new HttpResponse();
+
+        try {
+            clienteRepository.save(cliente);
+            response.setStatus(HttpStatus.OK);
+            response.setMessage("Produto atualizado com sucesso.");
+
+            return new ResponseEntity<>(response, response.getStatus());
+        }catch(Exception e) {
+            //talvez tratar erros com funções customizadas nos arquivos repository, ex: nome duplicado
+            //response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            //response.setMessage("Erro ao cadastrar o produto " + produto.getDescricao() + ". Motivo: Produto com esse nome já existe!");
+
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+
+            return new ResponseEntity<>(e.getMessage(), response.getStatus());
+        }
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public List<Cliente> remover(@RequestBody Cliente cliente) {
-        clienteRepository.deleteById(cliente.getId());
-        return clienteRepository.findAll(); 
+    public ResponseEntity<?> remover(@PathVariable("id") Long id) {
+        HttpResponse response = new HttpResponse();
+
+        try {
+            clienteRepository.deleteById(id);
+            response.setStatus(HttpStatus.OK);
+            response.setMessage("Cliente deletado com sucesso.");
+
+            return new ResponseEntity<>(response, response.getStatus());
+        }catch(Exception e) {
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+
+            return new ResponseEntity<>(e.getMessage(), response.getStatus());
+        }
     }
 }
